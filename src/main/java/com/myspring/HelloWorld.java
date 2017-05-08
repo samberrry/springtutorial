6cf93bf4e1c3c80806db4a4b5c6127e3a7af9de5
Hessam <hessam.modaberi@gmail.com>
2017-05-08 15:08:49 +0430

    bean definition inheritance

--- src/main/java/com/myspring/HelloWorld.java
+++ src/main/java/com/myspring/HelloWorld.java
@@ -4,15 +4,22 @@ package com.myspring;
  * Created by Hessam on 5/6/17.
  */
 public class HelloWorld {
-    private String message;
+    private String message1;
+    private String message2;
 
-    public void setMessage(String message) {
-        this.message = message;
+    public void setMessage1(String message1) {
+        this.message1 = message1;
     }
-    public void getMessage(){
-        System.out.println("Your message : "+ message);
+    public void getMessage1(){
+        System.out.println("World message1 : "+ message1);
     }
 
+    public void getMessage2() {
+        System.out.println("World message2 : "+ message2);
+    }
+    public void setMessage2(String message2) {
+        this.message2 = message2;
+    }
     public void init(){
         System.out.println("Bean is going through init.");
     }
--- src/main/java/com/myspring/MainApp.java
+++ src/main/java/com/myspring/MainApp.java
@@ -10,7 +10,13 @@ public class MainApp {
     public static void main(String[] args){
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-        obj.getMessage();
+        obj.getMessage1();
+        obj.getMessage2();
+
+        HelloIndia obj2 = (HelloIndia)context.getBean("helloIndia");
+        obj2.getMessage1();
+        obj2.getMessage2();
+        obj2.getMessage3();
 
     }
 }
