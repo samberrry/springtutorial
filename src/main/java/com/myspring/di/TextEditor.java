c61f4c0f82cd8c41e59660cfeaddfd6cd573e9e2
Hessam <hessam.modaberi@gmail.com>
2017-06-28 20:04:08 +0430

    autowired annotation constructor

--- src/main/java/com/myspring/di/TextEditor.java
+++ src/main/java/com/myspring/di/TextEditor.java
@@ -8,8 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class TextEditor {
     private SpellChecker spellChecker;
     private String name;
+
+    public TextEditor() {
+        System.out.println("Inside TextEditor constructor." );
+    }
+
     @Autowired
-    public void setSpellChecker(SpellChecker spellChecker){
+    public TextEditor(SpellChecker spellChecker) {
+        System.out.println("Inside TextEditor constructor." );
         this.spellChecker = spellChecker;
     }
 
@@ -21,9 +27,9 @@ public class TextEditor {
         this.name = name;
     }
 
-    public SpellChecker getSpellChecker() {
-        return spellChecker;
-    }
+//    public SpellChecker getSpellChecker() {
+//        return spellChecker;
+//    }
     public void spellCheck(){
         spellChecker.checkSpelling();
     }
